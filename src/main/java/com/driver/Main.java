fac75fd1b7a66d3618cff6432942416e3bc92dfa
Ataliya Aarfeen <ataliyaengg@gmail.com>
2023-06-24 22:16:24 +0530

    Initial Add

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Ataliya");
+        String name =rwOnly.getName();
+    }
+
 }
